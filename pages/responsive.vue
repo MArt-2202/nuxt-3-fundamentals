19c749d3e53ca10fa9593f7b751b84a41e9373e4
Artem <mart_22@ukr.net>
2024-11-21 19:09:00 +0200

    Add responsive.vue

---
+++ pages/responsive.vue
@@ -0,0 +1,47 @@
+<script setup>
+definePageMeta({
+	layout: "plain",
+});
+const modifiers = ref({
+	quality: 100,
+	flip: undefined,
+	flop: undefined,
+	sharpen: undefined,
+	blur: undefined,
+	negate: undefined,
+	tint: undefined,
+	grayscale: undefined,
+});
+
+const crop = ref({
+	left: 2213,
+	top: 402,
+	width: 800,
+	height: 800,
+});
+
+const extract = computed(() =>
+{
+	if (crop.value.width && crop.value.height)
+		return `${crop.value.left}_${crop.value.top}_${crop.value.width}_${crop.value.height}`;
+	return;
+});
+</script>
+<template>
+	<!-- <picture>
+		<source media="(max-width:650px)" srcset="/_ipx/extract_2213_402_800_800&w_1860&f_jpg/cat.jpg" />
+		<img src="/_ipx/_/cat.jpg" alt="" />
+	</picture> -->
+
+	<NuxtPicture src="/cat.jpg" sizes="sm:100vw sm:50vw md:100vw" format="avif, webp" />
+
+	<!-- <NuxtPicture src="/cat.jpg" sizes="sm:100vw sm:50vw md:100vw" :modifiers="{ ...modifiers, extract }"
+		format="avif, webp" /> -->
+</template>
+
+<style scoped>
+:deep(img) {
+	display: block;
+	max-width: 100%;
+}
+</style>
